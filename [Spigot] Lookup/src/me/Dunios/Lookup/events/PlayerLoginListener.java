0a337d3320763eb5d75db659d5041d53db2b97bf
Konrad Bachmann <bachmannapps@gmail.com>
2016-07-31 22:47:18 +0200

    added name change check

--- [Spigot] Lookup/src/me/Dunios/Lookup/events/PlayerLoginListener.java
+++ [Spigot] Lookup/src/me/Dunios/Lookup/events/PlayerLoginListener.java
@@ -1,5 +1,6 @@
 package me.Dunios.Lookup.events;
 
+import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -12,15 +13,21 @@ public class PlayerLoginListener implements Listener
 	@EventHandler
 	public void onJoin(PlayerJoinEvent e)
 	{
+		Bukkit.getConsoleSender().sendMessage("§8[§cLookup§8] §e" + e.getPlayer().getName() + " §7joined §8(§7" + e.getPlayer().getUniqueId() + "§8)");
 		if(!SPlayerManager.userExist(e.getPlayer().getUniqueId()))
 		{
 			SPlayerManager.createPlayer(e.getPlayer());
+			Bukkit.getConsoleSender().sendMessage("§8[§cLookup§8] §7Created profile for §e" + e.getPlayer().getName());
 		}
-		
-		SPlayer sp = SPlayerManager.getSPlayerByName(e.getPlayer().getName());
+		SPlayer sp = SPlayerManager.getSPlayerByUUID(e.getPlayer().getUniqueId());
 		sp.setOnline(true);
 		sp.setLastLogin(SPlayerManager.getCurrentDate());
 		sp.setJoinCount(sp.getJoinCount() + 1);
+		if(!e.getPlayer().getName().equalsIgnoreCase(sp.getName()))
+		{
+			Bukkit.getConsoleSender().sendMessage("§8[§cLookup§8] §e" + sp.getName() + "§7 changed his name to §e" + e.getPlayer().getName());
+			sp.setName(e.getPlayer().getName());
+		}
 		sp.save();
 	}
 }
